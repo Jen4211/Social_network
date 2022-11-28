86b27d0680ac7cc009775e6603ced87191078456
Yauheni <jengulya4211@gmail.com>
2022-11-29 00:11:00 +0300

    feat: add route and navLink

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+   <BrowserRouter>
     <div className="app__wrapper">
       <Header />
       <Navbar />
       <div className="wrapp__profile">
-      <Profile />
-      {/* <Dialogs /> */}
+      <Routes>
+        <Route path='/dialogs' element={<Dialogs />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
       </div>
     </div>
+   </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Navbar = (props) => {
    return(
       <nav className={style.nav}>
       <ul className={style.list}>
-        <li className={style.item}>Profile</li>
-        <li className={style.item}>Messages</li>
-        <li className={style.item}>News</li>
-        <li className={style.item}>Music</li>
-        <li className={style.item}>Settings</li>
+        <li className={style.item}>
+          <NavLink to='/profile'>Profile</NavLink>
+        </li>
+        <li className={style.item}>
+          <NavLink to='/dialogs'>Messages</NavLink>
+        </li>
+        <li className={style.item}>
+          <NavLink to='/news'>News</NavLink>
+        </li>
+        <li className={style.item}>
+          <NavLink to='/music'>Music</NavLink>
+        </li>
+        <li className={style.item}>
+          <NavLink to='/settings'>Settings</NavLink>
+        </li>
       </ul>
     </nav>
    )
