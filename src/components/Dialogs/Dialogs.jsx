ef2d7be8b185d8069ef0311bfe706c4ee66dc285
Yauheni <jengulya4211@gmail.com>
2022-11-29 22:37:56 +0300

    feat: add link in navbar and dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Dialogs = (props) => {
    return (
-      <div>
-         dialogs
+      <div className={style.dialogs__wrap}>
+         <div className={style.dialogs__list}>
+            <div className={style.dialog}>
+               <NavLink to='dialog/1' className={navData => navData.isActive ? style.active : style.color }>Sveta</NavLink>
+            </div>
+            <div className={style.dialog}>
+               <NavLink to='dialog/2' className={navData => navData.isActive ? style.active : style.color }>Hleb</NavLink>
+            </div>
+            <div className={style.dialog}>
+               <NavLink to='dialog/3' className={navData => navData.isActive ? style.active : style.color }>Jenia</NavLink>
+            </div>
+            <div className={style.dialog}>
+               <NavLink to='dialog/4' className={navData => navData.isActive ? style.active : style.color }>Sasha</NavLink>
+            </div>
+            <div className={style.dialog}>
+               <NavLink to='dialog/5' className={navData => navData.isActive ? style.active : style.color }>Vova</NavLink>
+            </div>
+            <div className={style.dialog}>
+               <NavLink to='dialog/6' className={navData => navData.isActive ? style.active : style.color }>Dunik Chert</NavLink>
+            </div>
+         </div>
+         <div className={style.messages__list}>
+            <div className={style.message}>Hi</div>
+            <div className={style.message}>How is your curse?</div>
+            <div className={style.message}>Yo</div>
+         </div>
       </div>
+
    );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,19 +8,19 @@ const Navbar = (props) => {
       <nav className={style.nav}>
       <ul className={style.list}>
         <li className={style.item}>
-          <NavLink to='/profile'>Profile</NavLink>
+          <NavLink to='/profile' className={navData => navData.isActive ? style.active : style.item }>Profile</NavLink>
         </li>
         <li className={style.item}>
-          <NavLink to='/dialogs'>Messages</NavLink>
+          <NavLink to='/dialogs' className={navData => navData.isActive ? style.active : style.item }>Messages</NavLink>
         </li>
         <li className={style.item}>
-          <NavLink to='/news'>News</NavLink>
+          <NavLink to='/news' className={navData => navData.isActive ? style.active : style.item }>News</NavLink>
         </li>
         <li className={style.item}>
-          <NavLink to='/music'>Music</NavLink>
+          <NavLink to='/music' className={navData => navData.isActive ? style.active : style.item }>Music</NavLink>
         </li>
         <li className={style.item}>
-          <NavLink to='/settings'>Settings</NavLink>
+          <NavLink to='/settings' className={navData => navData.isActive ? style.active : style.item }>Settings</NavLink>
         </li>
       </ul>
     </nav>
